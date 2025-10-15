cca9bed7978d4fa199e824c4be85c343b318b31d
cozzyd <cozzyd@kicp.uchicago.edu>
2025-10-15 16:38:55 -0500

    let read-packets add to db

--- test/read-packets.c
+++ test/read-packets.c
@@ -10,6 +10,16 @@ int main(int nargs, char ** args)
 
   pueo_handle_t h;
   pueo_handle_init(&h, nargs > 1 ? args[1] : "test.wfs","r");
+
+  pueo_db_handle_t * db = NULL;
+  if (nargs > 2)
+  {
+    db = pueo_db_handle_open(args[2],0);
+    if (!db)
+    {
+      fprintf(stderr,"Trouble opening DB handle %s\n", args[2]);
+    }
+  }
   pueo_packet_t * packet = 0;
   while (true)
   {
@@ -17,6 +27,7 @@ int main(int nargs, char ** args)
     printf("read: %d\n", read);
     if (read <= 0) break;
     pueo_dump_packet(stdout,packet);
+    if (db) pueo_db_insert_packet(db, packet);
   }
   return 0;
 }
