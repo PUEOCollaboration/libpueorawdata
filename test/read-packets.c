a8971603aecfae6dc1eb140e7e84cae868d1cc1e
Philipp Windischhofer <philipp.windischhofer@gmail.com>
2025-12-03 14:53:30 -0600

    Keith added sun-sensors db fix and Cosmin was editing the read-packets script

--- test/read-packets.c
+++ test/read-packets.c
@@ -1,6 +1,12 @@
+#define _GNU_SOURCE 
 #include "pueo/rawdata.h"
 #include "pueo/rawio.h"
 #include <stdbool.h>
+#include <stdint.h>
+#include <time.h>
+#include <stdio.h>
+
+#include <stdlib.h>
 
 
 int main(int nargs, char ** args)
@@ -10,7 +16,7 @@ int main(int nargs, char ** args)
 
   pueo_handle_t h;
   pueo_handle_init(&h, nargs > 1 ? args[1] : "test.wfs","r");
-
+  const char * outdir = NULL;
   pueo_db_handle_t * db = NULL;
   if (nargs > 2)
   {
@@ -20,6 +26,14 @@ int main(int nargs, char ** args)
       fprintf(stderr,"Trouble opening DB handle %s\n", args[2]);
     }
   }
+
+
+  if (nargs > 3) 
+  {
+    outdir = args[3];
+
+  }
+
   pueo_packet_t * packet = 0;
   printf("{\n");
   while (true)
@@ -29,6 +43,19 @@ int main(int nargs, char ** args)
     if (read <= 0) break;
     pueo_dump_packet(stdout,packet);
     if (db) pueo_db_insert_packet(db, packet);
+    if (outdir) 
+    {
+      char * ofile = NULL;
+      struct timespec now;
+      clock_gettime(CLOCK_REALTIME,&now);
+      asprintf(&ofile,"%s/%d.%09d.dat", outdir, now.tv_sec, now.tv_nsec);
+      pueo_handle_t hout;
+      pueo_handle_init(&hout, ofile, "w");
+      pueo_ll_write(&hout, packet->head.type, packet->payload);
+      pueo_handle_close(&hout);
+    }
+    free(packet);
+    packet = 0;
   }
   printf("}\n");
   return 0;
