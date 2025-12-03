2ddbcf7dec5aafca321e0ab2f84cc9b3de871752
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2025-12-04 09:20:40 +1300

    untested plotter for single wfs and executable

---
+++ test/plot_single_wfs.cc
@@ -0,0 +1,66 @@
+#include "pueo/rawdata.h"
+#include "pueo/rawio.h"
+#include "TCanvas.h"
+#include "TGraph.h"
+#include <cstdio>
+#include "TAxis.h"
+#include "TStyle.h"
+
+void plot_single_wfs(const char * infile, const char * out_file)
+{
+
+  gStyle->SetLineScalePS(1);
+  pueo_handle_t h;
+  pueo_handle_init(&h, infile,"r");
+
+  pueo_single_waveform_t wf;
+  if (pueo_read_single_waveform(&h, &wf) <= 0)
+  {
+    printf("Hmm... didn't get any bytes?\n");
+    return;
+  }
+
+  double subs = double(wf.event_time - wf.last_pps) / double(wf.last_pps - wf.llast_pps);
+  printf("Subsecond: %f\n", subs );
+
+  TCanvas * c = new TCanvas("c","c", 1800, 1000);
+  c->cd();
+
+  TGraph * g = new TGraph(wf.wf.length);
+  for (int i = 0; i < g->GetN(); i++)
+  {
+    g->SetPoint(i,i/3., wf.wf.data[i]);
+  }
+
+  g->SetTitle(Form("Run %d Event %d CH %d (subsecond=%f), RMS=%f;ns;adc", wf.run, wf.event, wf.wf.channel_id, subs, g->GetRMS(2)));
+  g->GetYaxis()->CenterTitle();
+  g->GetXaxis()->CenterTitle();
+  g->GetXaxis()->SetRangeUser(0,1024/3.);
+  g->Draw("al");
+
+  gPad->SetGridx();
+  gPad->SetGridy();
+  if (out_file)
+  {
+    c->SaveAs(out_file);
+  }
+
+}
+
+
+int main(int nargs, char ** args) 
+{
+  if (nargs < 3) 
+  {
+    printf("Usage: plot_single_wfs input.dat output.png\n");
+    return 1;
+  }
+
+  plot_single_wfs(args[1], args[2]);
+
+
+  return 0;
+}
+
+
+
