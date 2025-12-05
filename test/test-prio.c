55e42e712020ee4f1c9cf24789b369702f296a32
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2025-12-05 02:30:35 +0000

    updateeeeee

--- test/test-prio.c
+++ test/test-prio.c
@@ -1,10 +1,9 @@
 
-#include <../inc/pueo/prio_interface.h>
-#include <../inc/pueo/rawio.h>
+#include <pueo/prio_interface.h>
+#include <pueo/rawio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 
 #define BATCH_SIZE 
 
@@ -23,115 +22,86 @@ int main(int nargs, char ** args)
 
   printf("Prioritizer Initialized\n");
 
-  pueo_prio_cfg_opt_t c = prio.pueo_prio_configure();
+  pueo_prio_cfg_opt_t c;
+
+  c.noiseThreshold = 0.158; //if above this, drop into Starlink Bin
+  c.tdrsSignalThreshold = 0.138; //if above this, drop into TDRSS Bin
+  c.extraSignalThreshold = -1; //Another signal value if desired
+  c.rmsThreshold = 16; //if channel RMS exceeds this value, count as "loud"
+  c.fullBlastThreshold = 87; //number of channels that must be "loud" to classify as full payload blast
+
+  c.topBlastThreshold = 1.5; //Threshold ratio of pkpk for largest top ring / largest bottom ring
+  c.bottomBlastThreshold = 0.5;
+  c.frontBackThreshold = 12; //Threshold ratio of pkpk for largest front antenna / largest back antenna
+  c.aboveHorizontal = 5; //in deg, sets all signals that reconstruct above this elevation as noise
+
+  c.scRiseTimeLowLim = 0.4; //Lower limit for rise time integration
+  c.scRiseTimeUpLim = 0.7; //Upper limit for rise time integration
+
+  c.lowpass = 750; //in MHz, lowpass cutoff
+  c.highpass = 100; //in MHz, highpass cutoff
+
+  //For Filter Kernel, all values in MHz. For multiple notches, do "XX, XX, ..."
+  c.filterCenters = "370";
+  c.filterSpans = "10";
+
+
+  prio.pueo_prio_configure(&c);
 
   printf("Prioritizer Configured\n");
 
-  const char * wf_file = "../rachel4.wfs";///mnt/storagea/wfs/run0138/002000.wfs.dat.zstd";
-  pueo_handle_t h;
-  pueo_handle_init(&h, wf_file, "r");
+  char base_filename[] = "/mnt/storagea/wfs/run0200/0";
+  char extension[] = "00.wfs.dat.zstd";
+  char wf_file[46]; // Buffer to hold the full filename
 
-  static pueo_full_waveforms_t all_wfs[PUEO_PRIO_MAX_BATCH] = {0};
-  //pueo_prio_result_t * results[PUEO_PRIO_MAX_BATCH];
-  static pueo_prior_Event_t events[PUEO_PRIO_MAX_BATCH];
+  int prior_1=0;
+  int prior_2=0;
 
   int n_in_batch = 100;
-  int n = 0;
-  while (true)
-  {
-    int read = pueo_read_full_waveforms(&h, &all_wfs[n]);
-    //printf("read: %d\n", read);
-    if (read <= 0) break;
-    //pueo_dump_full_waveforms(stdout,&all_wfs[0]);
-    memcpy(&events[n].allwfms, &all_wfs[n], sizeof(all_wfs[n]));
-    //printf("%d\n",all_wfs[0].wfs[12].data[100]);
-    events[n].trigL2 = 0;
-    n++;
-  }
-
-  /*for (int n=0; n<n_in_batch; n++){
-      int read = pueo_read_full_waveforms(&h, &all_wfs[0]);
-      printf("read: %d\n", read);
-      pueo_dump_full_waveforms(stdout, &all_wfs[0]);
-      memcpy(&events[n].allwfms, &all_wfs[n], sizeof(all_wfs[n]));
-      printf("%d\n",all_wfs[n].wfs[12].data[100]);
-      events[n].trigL2 = 0;
-    }*/
   
-  //printf("%d\n",events[0].trigL2);
-  
-  for (int run=0; run<1; run++){
-    pueo_prio_result_t* results = prio.pueo_prio_compute(events, n_in_batch, c);
-    if (run == 0) {
-      for (int ch=0; ch<192; ch++){
-        //printf(" result { ch = %d, .rms = %f, pkpk = %f}\n",ch, results[0].rms[ch],results[0].pk2pk[ch]);
-      }
-    }
-  }
-  
-  for (int n=0; n<n_in_batch; n++){
-    //printf(" result { .phi = %f, .theta = %f, .imp = %f, .map_peak = %f, .rms = %f, .pkpk = %f }\n", results[n].phi, results[n].theta, results[n].imp, results[n].map_peak, results[n].rms[191], results[n].pk2pk[191]);
-  }  
-  
-  for (int ch=0; ch<192; ch++){
-    //printf(" result { ch = %d, .rms = %f, pkpk = %f}\n",ch, results[0].rms[ch],results[0].pk2pk[ch]);
-  }
-  /*if (nargs < 3)
-  {
-    fprintf(stderr,"test-prio prio.so  wffile.dat");
-    return 1;
-  }
-
-  const char * sofile = args[1];
-  const char * wf_file = args[2];
-
-  pueo_prio_impl_t prio;
-  if (pueo_prio_create(sofile, &prio))
-  {
-    fprintf(stderr,"Coudln't load prio from %s\n", sofile);
-    return 1;
-  }
-
   pueo_handle_t h;
-  pueo_handle_init(&h, wf_file, "r");
-
-  pueo_full_waveforms_t wfs[PUEO_PRIO_MAX_BATCH];
-  pueo_prio_result_t results[PUEO_PRIO_MAX_BATCH];
-
-
-  int n_in_batch = 0;
-
-  prio.pueo_prio_init();
-  pueo_prio_cfg_opt_t opts[] = {
-    { .key = "max-batch", .val = {.uval = PUEO_PRIO_MAX_BATCH } }
-  };
-  prio.pueo_prio_configure( 1, opts);
-
-  while (true)
-  {
-
-    int rd = pueo_read_full_waveforms(&h, &wfs[n_in_batch]);
-
-    if (rd > 0) n_in_batch++;
-
-    if (rd <= 0 || n_in_batch == PUEO_PRIO_MAX_BATCH)
+  for (int i=1; i<35; i++){
+    snprintf(wf_file, sizeof(wf_file), "%s%03d%s", base_filename, i, extension);
+    
+    static pueo_full_waveforms_t all_wfs[PUEO_PRIO_MAX_BATCH] = {0};
+    static pueo_prior_Event_t events[PUEO_PRIO_MAX_BATCH];
+    
+    pueo_handle_init(&h, wf_file, "r");
+    int n = 0;
+    while (true)
     {
-
-      if (n_in_batch)
-      {
-        prio.pueo_prio_compute(n_in_batch, wfs, results);
-
-        for (int i = 0; i < n_in_batch; i++)
-        {
-          printf(" result { .phi = %f, .theta = %f, .imp = %f, .map_peak = %f }\n", results[i].phi, results[i].theta, results[i].imp, results[i].map_peak);
-        }
-        n_in_batch = 0;
-      }
-
-      if (rd <= 0) break;
+      int read = pueo_read_full_waveforms(&h, &all_wfs[n]);
+      //printf("read: %d\n", read);
+      if (read <= 0) break;
+      memcpy(&events[n].allwfms, &all_wfs[n], sizeof(all_wfs[n]));
+      events[n].trigL2 = 0;
+      n++;
     }
-  }*/
 
+    for (int run=0; run<1; run++){
+      pueo_prio_result_t* results = prio.pueo_prio_compute(events, n_in_batch, c);
+      if (run == 0) {
+        for (int ch=0; ch<192; ch++){
+          //printf(" result { ch = %d, .rms = %f, pkpk = %f}\n",ch, results[0].rms[ch],results[0].pk2pk[ch]);
+        }
+      }
+    
+      for (int n=0; n<n_in_batch; n++){
+        //printf(" result { .phi = %f, .theta = %f, .imp = %f, .map_peak = %f, .rms = %f, .pkpk = %f }\n", results[n].phi, results[n].theta, results[n].imp, results[n].map_peak, results[n].rms[191], results[n].pk2pk[191]);
+        //printf(" result { Rise Time Priority = %d, Front/Back Blast = %d, Full Payload Blast = %d, Top Ring Blast = %d, Bottom Ring Blast = %d} \n", results[n].priority.signal_level, results[n].priority.frontback_blast_flag, results[n].priority.fullpayload_blast_flag, results[n].priority.topring_blast_flag, results[n].priority.botring_blast_flag);
+        //printf("Rise Time Priority = %d, Az = %f, Al = %f, Rise Time = %f \n", results[n].priority.signal_level, results[n].phi, results[n].theta, results[n].imp);
+        if (results[n].priority.signal_level == 1){
+          prior_1 += 1;
+        }
+        if (results[n].priority.signal_level == 2){
+          prior_2 += 1;
+        }
+      }
+    }
+  }
+
+  printf("Priority 1: %d \n",prior_1);
+  printf("Priority 2: %d \n",prior_2); 
   return 0;
 
 }
