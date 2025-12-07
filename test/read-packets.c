0a475c21b2d33457fd5e2b8332655b1aebcb294d
Philipp Windischhofer <philipp.windischhofer@gmail.com>
2025-12-06 20:47:58 -0600

    read-packets can append to file or write to directory

--- test/read-packets.c
+++ test/read-packets.c
@@ -1,22 +1,28 @@
 #define _GNU_SOURCE
 #include "pueo/rawdata.h"
 #include "pueo/rawio.h"
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <time.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include <stdlib.h>
 
+int is_directory(const char* path)
+{
+  struct stat path_stat;
+  stat(path, &path_stat);
+  return S_ISDIR(path_stat.st_mode);
+}
 
 int main(int nargs, char ** args)
 {
-
-
-
   pueo_handle_t h;
   pueo_handle_init(&h, nargs > 1 ? args[1] : "test.wfs","r");
-  const char * outdir = NULL;
+  char* outpath = NULL;
 
   pueo_db_handle_t * db = NULL;
   if (nargs > 2)
@@ -30,7 +36,7 @@ int main(int nargs, char ** args)
 
   if (nargs > 3)
   {
-    outdir = args[3];
+    outpath = args[3];
   }
 
   pueo_packet_t * packet = 0;
@@ -38,17 +44,29 @@ int main(int nargs, char ** args)
   while (true)
   {
     int read = pueo_ll_read_realloc(&h, &packet);
-//    fprintf(stderr,"read: %d\n", read);
     if (read <= 0) break;
     pueo_dump_packet(stdout,packet);
     if (db) pueo_db_insert_packet(db, packet);
-    if (outdir) {
-      char * ofile = NULL;
-      struct timespec now;
-      clock_gettime(CLOCK_REALTIME,&now);
-      asprintf(&ofile,"%s/%d.%09d.dat", outdir, now.tv_sec, now.tv_nsec);
+
+    if (outpath) {
+      char* ofile = NULL;
+      char* mode = NULL;
+
+      if(is_directory(outpath)) {
+        // We're given a directory, so make up a filename
+        struct timespec now;
+        clock_gettime(CLOCK_REALTIME,&now);
+        asprintf(&ofile,"%s/%d.%09d.dat", outpath, now.tv_sec, now.tv_nsec);
+        asprintf(&mode, "w");
+      }
+      else {
+        // We're given a full filename, append to that location
+        ofile = outpath;
+        asprintf(&mode, "a");
+      };
+
       pueo_handle_t hout;
-      pueo_handle_init(&hout, ofile, "w");
+      pueo_handle_init(&hout, ofile, mode);
       pueo_ll_write(&hout, packet->head.type, packet->payload);
       pueo_handle_close(&hout);
     } 
