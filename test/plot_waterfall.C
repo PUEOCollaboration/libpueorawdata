d4b7de5cc79e2de542fc620039fad4b91bb5b67a
jason yao <yao.966@osu.edu>
2026-01-08 01:35:26 +0000

    use RDataFrame?

--- test/plot_waterfall.C
+++ test/plot_waterfall.C
@@ -1,37 +1,37 @@
 #include "pueo/rawio.h"
 #include "FTPair.h"
-#include "TGraph.h"
-#include "TH2D.h"
-#include "TCanvas.h"
+#include "ROOT/RDataFrame.hxx"
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
+namespace { // because we love globals
+  const ROOT::RDF::ColumnNames_t branchNames{"readout time (utc sec)", "power spectrum (db)"};
+}
 
-void plot_waterfall(){
+// horrendously slow, but oh well
+void save_power_spectrum_using_RDataFrame(const std::string& rawDataFileName, const std::string& rootFileName) {
 
-  pueo_handle_t fwf_file_handle;
-  char * filename = NULL;
-  asprintf(&filename, "2025-12-31-R005.wfs");
-  int handleinit = pueo_handle_init(&fwf_file_handle, filename, "r");
-  if (handleinit < 0) {
-    fprintf(stderr, "Bad handle (filename: %s, error code: %d)", filename, handleinit);
-    free(filename);
+  pueo_handle_t rawDataFileHandle; // make sure this file contains pueo_full_waveforms_t and not other packet types
+  int err = pueo_handle_init(&rawDataFileHandle, rawDataFileName.c_str(), "r");
+  if (err < 0) {
+    fprintf(stderr, "Bad handle (filename: %s, error code: %d)", rawDataFileName.c_str(), err);
     exit(1);
   }
 
   pueo_full_waveforms_t fwf;
-  struct tm* time_utc = NULL; // don't free
-  // while (true) {
-    int read_status = pueo_read_full_waveforms(&fwf_file_handle, &fwf);
-    if (read_status < 0) {
-      // break;
-    }
+  const char treename[] = "waveform tree";
+  TTree fwf_tree(treename, "");
+
+  uint32_t readoutTime;
+  TGraph powerSPectrum;
+  fwf_tree.Branch(branchNames[0].c_str(), &readoutTime);
+  fwf_tree.Branch(branchNames[1].c_str(), &powerSPectrum);
+
+  while (true) {
+    int read_status = pueo_read_full_waveforms(&rawDataFileHandle, &fwf);
+    if (read_status < 0) break;
   
-    const time_t readout_time  = fwf.readout_time.utc_secs;
-    time_utc = gmtime(&readout_time);
-    char time_buffer[24];
-    strftime(time_buffer, sizeof(time_buffer), "%Y-%m-%d %H:%M:%S UTC", time_utc);
+    readoutTime = fwf.readout_time.utc_secs;
 
     TGraph time_domain(fwf.wfs[0].length);
     for (int i = 0; i < time_domain.GetN(); i++)
@@ -39,23 +39,21 @@ void plot_waterfall(){
       time_domain.SetPoint(i, i/3., fwf.wfs[0].data[i]);
     }
 
-    time_domain.SetTitle(
-      Form("Run %d, Event %d, CH %d, %s", fwf.run, fwf.event, fwf.wfs[0].channel_id, time_buffer)
-    );
-
     nicemc::FTPair wf_ftpair(time_domain);
-    TGraph freq_domain = wf_ftpair.makePowerSpectrumGraph();
-    for (int i=0; i<freq_domain.GetN(); i++){
-      double power_i = freq_domain.GetPointY(i);
-      double db = 10 * std::log10(power_i);
-      freq_domain.SetPointY(i, db);
+    powerSPectrum = wf_ftpair.makePowerSpectrumGraph();
+    for (int i=0; i<powerSPectrum.GetN(); i++){
+      double db = 10 * std::log10(powerSPectrum.GetPointY(i));
+      powerSPectrum.SetPointY(i, db);
     }
 
-    TCanvas c1("", "", 1920, 1080);
-    TH2D waterfall_hist("", Form("Run %u", fwf.run), 100, 0, 100, 100, 0, 100);
-    waterfall_hist.Draw("colz");
-    c1.SaveAs("foobar.svg");
-  // }
+    fwf_tree.Fill();
+  }
 
-  free(filename);
+  ROOT::RDataFrame rdf(fwf_tree, branchNames);
+  rdf.Snapshot(treename, rootFileName);
+}
+
+void plot_waterfall(){
+  std::string rootFileName("full_waveform.root");
+  save_power_spectrum_using_RDataFrame("2025-12-31-R005.wfs", rootFileName);
 }
