81b1d5b46088cb41265f9c596c17e58bdcaca0af
jason yao <yao.966@osu.edu>
2026-02-20 17:26:09 +0000

    `find_dependency()`

--- cmake/pueorawdataConfig.cmake
+++ cmake/pueorawdataConfig.cmake
@@ -2,5 +2,5 @@ include("${CMAKE_CURRENT_LIST_DIR}/pueorawdataTargets.cmake")
 
 include(CMakeFindDependencyMacro)
 find_dependency(ZLIB REQUIRED)
-find_package(PostgreSQL)
-find_package(SQLite3)
+find_dependency(PostgreSQL)
+find_dependency(SQLite3)
